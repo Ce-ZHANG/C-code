e5bd3a6c8209bbd7879e3a3408283b3efc460f98
Francis Zhang <41145185+Ce-ZHANG@users.noreply.github.com>
2021-05-06 23:22:10 +1000

    Recording some projects I have done before
    Numerical Programming for Engineer based on C code
    Instantaneous flow field calculation based on big data, including Maximum Velocity Difference, Mean Velocities on a Coarser Grid, Searching in Data Structures.

---
+++ tasks.c
@@ -0,0 +1,35 @@
+/***************************************************************************
+ *
+ *   File        : tasks.c
+ *   Student Id  : <INSERT STUDENT ID HERE>
+ *   Name        : <INSERT STUDENT NAME HERE>
+ *
+ ***************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//#include <sys/time.h>
+#include <string.h>
+#include "tasks.h"
+
+
+
+void maxveldiff(const char* flow_file)
+{
+    printf("maxveldiff() - IMPLEMENT ME!\n");
+}
+
+void coarsegrid(const char* flow_file, int resolution)
+{
+    printf("coarsegrid() - IMPLEMENT ME!\n");
+}
+
+void searching(const char* flow_file)
+{
+    printf("searching() - IMPLEMENT ME!\n");
+}
+
+void vortcalc(const char* flow_file)
+{
+    printf("vortcalc() - IMPLEMENT ME!\n");
+}
